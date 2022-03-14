4046e3c2218a90d80155fa9bc0b312ae00baeeee
sayapinavalentina <sayapinavalentina7@gmail.com>
2022-03-14 12:58:12 +0300

    Initial Commit

---
+++ WorldCinema_Sayapina/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WorldCinema_Sayapina
+//
+//  Created by Student on 14.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
